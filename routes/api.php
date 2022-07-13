865bef73e868e34c3ee5e11a8d0a36ba1ccd6d2e
Fabiano Matozinhos <fabianomatozinhos1@hotmail.com>
2022-07-13 00:24:24 -0300

    trabalhando com laravel

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\SeriesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/series', [SeriesController::class, 'index']);
+Route::post('/series',[SeriesController::class, 'store']);
+Route::get('/series/{serie}',[SeriesController::class, 'show']);
+Route::put('/series/{serie}',[SeriesController::class, 'update']);
+Route::delete('/series/{serie}',[SeriesController::class, 'destroy']);
